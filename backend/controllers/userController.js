affc0e909d100f36661153fb8f6835723cecb1e7
Shahwez Ahmad <shahwez@Shahwezs-MacBook-Air.local>
2025-04-05 01:14:15 +0530

    adding errorMiddleware install express-async-handle

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,20 @@
+const asyncHandler = require('express-async-handler')
+// @desc - for register new user on route (/api/users/)
+const registerUser =    asyncHandler(async (req,res) => {
+   const {name, email, password} = req.body
 
+   if(!name || !email || !password) {
+    res.status(400)
+    throw new Error('Enter all field')
+   }
+    res.send('hello')
+})
 
-const registerUser = (req,res) => {
-    res.send('Register from controller')
-}
+// @desc - for logi user on route (/api/users/login)
 
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async(req, res) => {
     res.send('Login user from controller')
-}
+})
 
 module.exports = {
      registerUser,
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 8000
-
+const {errorHandler} = require('./middleware/errorMiddleware.js')
 const app = express()
-
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))  
 app.get('/', (req, res ) =>{
     res.status(500).send("helooo")
 })
 
 app.use('/api/users', require('./routes/userRoute'))
-
+app.use(errorHandler)
 app.listen(port, () => console.log(`App is running or ${port}`))

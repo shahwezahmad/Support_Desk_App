a5c7677a055ffb6cd8cd6b15f98a741fa8987aed
Shahwez Ahmad <shahwez@Shahwezs-MacBook-Air.local>
2025-04-04 21:24:55 +0530

    basic server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("check update......")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/', (req, res ) =>{
+    res.status(500).send("helooo")
+})
+
+app.listen(port, () => console.log(`App is running or ${port}`))
